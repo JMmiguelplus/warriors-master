6876451a01e594847e1d71d6218f30aeb5357996
JMmiguelplus <migue_luke@hotmail.com>
2019-02-19 17:31:54 -0600

    nuevos avances,footer,nuevos campos y la nueva ventana de activacion de licecnias

---
+++ Active licenses/check.php
@@ -0,0 +1,32 @@
+<?php
+sleep(2);
+if (empty($_POST['licencia'])) {
+  echo "101";
+}else{
+  $cadena ="host='localhost' port='5432' dbname='registros' user='postgres' password='12345'";
+  // creación de la conexión a la base de datos con pg_connect()
+  $conexion = pg_connect($cadena) or die ("No se ha podido conectar al servidor de Base de datos");
+  $lic = $_POST['licencia'];
+  $sql = "SELECT * FROM datos1 where codigo = '".$lic."'";
+  $res = pg_query($conexion,$sql)  or die ("Algo ha ido mal en la consulta a la base de datos");
+  $rows = pg_num_rows($res);
+
+if (!$res) {
+  echo "102";
+}
+  if ($rows == 1) {
+    while ($row = pg_fetch_row($res)) {
+      $reg['id'] = $row[0];
+      $reg['nom'] = $row[1];
+      $reg['proc'] = $row[4];
+      $reg['fci'] = $row[5];
+      $reg['fcf'] = $row[6];
+      $reg['lic'] = $row[7];
+      $reg['tim'] = $row[8];
+    }
+    echo json_encode($reg);
+  }else {
+    echo "103";
+  }
+}
+?>
---
+++ Active licenses/index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="icon" type="img/ico" sizes="16x16" href="img/favicon.ico">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Warriors Estatus</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+  <!-- Plugin CSS -->
+  <link rel="stylesheet" href="device-mockups/device-mockups.min.css">
+  <script src="vendor/jquery/jquery.min.js"></script>
+
+  <!-- Custom styles for this template -->
+  <link href="css/new-age.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/estilos.css">
+  <script>
+  $(document).ready(function(){
+    $("#insert").modal("show");
+  });
+
+  </script>
+</head>
+
+<body id="page-top">
+
+  <div class="modal" tabindex="-1" data-backdrop="static" data-keyboard="false" role="dialog" id="insert">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Activa tu licencia</h5>
+      </div>
+      <div class="modal-body">
+      <form>
+        <div class="form-group">
+          <label for="recipient-name" class="col-form-label">Coloca tu licencia</label>
+          <input type="text" class="form-control" id="activar">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <div id="alert"><img id="imagen" src="img/cargando.gif" ><span id="mensaje"></span></div>
+        <button type="button" class="btn btn-outline-success" id="check" value="Verificar" >Activar</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+
+  $(document).ready(function(){//funcion para comprobar el codigo de la licencia
+      $("#check").click(function(){
+        var lic = document.getElementById('activar').value;
+           $.ajax({
+             type:"POST",
+             url:"check.php",
+             data:{licencia:lic},
+             dataType: "JSON",
+             beforeSend: function() {
+               $("#check").val("Comprobando...");
+               $("#imagen").show();
+               $("#mensaje").html('Activando..');
+             }
+           })
+           .done(function(respuesta){
+             $('#imagen').hide();
+             if(respuesta == 1){
+               $('mensaje').html('hola');
+             }
+             else{
+               $('#mensaje').html('Ingresa un Codigo');
+             }
+           }
+         )
+           .done(function (res) {
+             if (res == 101) {
+             } else if(res == 102) {
+               alert('Error al ejecutar su consulta')
+             } else if(res == 103) {
+               alert('La licencia '+lic+' no concuerda con ninguna en el sistema. Por favor vuelva a ingresar su licencia correctamente.');
+             } else {
+               $('#check').val('Verificar');
+               $('#insert').modal('hide');
+               $('#table-one').val('show');
+               $('#nomb').html(res.nom);
+               $('#pro').html(res.proc);
+               $('#feci').html(res.fci);
+               $('#fecf').html(res.fcf);
+               $('#lice').html(res.lic);
+               $('#time').html(res.tim);
+           }
+         });
+       })
+     })
+
+
+
+      </script>
+
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-lg-7 my-auto">
+          <div class="header-content mx-auto">
+          </div>
+          <table class="table table-sm table-responsive table-bordered table-dark" id="table-one" name="table-one">
+          <thead>
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Licencia</th>
+              <th scope="col">Equipo</th>
+              <th scope="col">Tiempo</th>
+              <th scope="col">Fecha inicial</th>
+              <th scope="col">Fecha Final</th>
+              <th scope="col">*Servicios</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td rowspan="3" id="nomb"></td>
+              <td rowspan="3"id="lice"></td>
+              <td rowspan="3"id="pro"></td>
+              <td rowspan="3"id="time"></td>
+              <td rowspan="3"id="feci"></td>
+              <td rowspan="3"id="fecf"></td>
+              <td class="bg-success">Soporte</td>
+            </tr>
+            <td class="bg-success">Mesa de ayuda</td>
+          </tr>
+          <tr>
+          <td class="bg-danger">Actuliazaciones</td>
+
+            </tr>
+            </table>
+          </tbody>
+       </div>
+        <div class="col-lg-5 my-auto">
+          <div class="device-container">
+                <div class="screen">
+                  <img src="img/logo2.png" class="img-fluid" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <footer>
+    <div class="container">
+      <p>&copy; Your Website 2019. All Rights Reserved.</p>
+      <ul class="list-inline">
+        <li class="list-inline-item">
+          <a href="#">Privacy</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#">Terms</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#">FAQ</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <!--<script src="js/new-age.min.js"></script>-->
+
+</body>
+
+</html>
